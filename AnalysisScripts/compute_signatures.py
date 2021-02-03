cleaned up computation of epsilon, kappa, eta, corrected minor bugs in result analysis, etc

AnalysisScripts/compute_signatures.py
@@ -91,7 +91,8 @@ def compute_all_signatures(root_dir,num_pts=16,append_path=[[0, -24]],num_gens_t
                     if bd_full.tolist() not in seen:
                         seen.append(bd_full.tolist())
 
-                    sig=iis.sig(bd_full,2)
+                    sig=iis.sig(bd_full,3)
+                    #pdb.set_trace()
                     sig_list.add(tuple(sig.tolist()))
 
     sig_list=list(sig_list) 
@@ -102,67 +103,44 @@ def compute_all_signatures(root_dir,num_pts=16,append_path=[[0, -24]],num_gens_t
 
 if __name__=="__main__":
     
-    num_gens_to_check=1200
+    num_gens_to_check=1000
 
     if 1:
 
-        root_archive_based="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/archive_4000//NS_log_87678"
+        root_archive_based="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/archive_based/NS_log_34498/"
         sigs_a, seen_a=compute_all_signatures(root_archive_based,num_gens_to_check=num_gens_to_check)
-        for x in sigs_a:
-            plt.plot(x,"r")
-        #plt.show()
 
     if 0:
 
+        #sigs_a=np.load("/tmp/sigs_a_4000.npy")
         sigs_a=np.load("/tmp/sigs_a_6000.npy")
 
     
-    if 0:
+    if 1:
         #root_learnt="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/learnt/NS_log_67193"
         #root_learnt="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/learnt/NS_log_67193"
         #root_learnt="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/learnt/NS_log_11048"
+        root_learnt="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/learnt/NS_log_1605/"
         sigs_l, seen_l=compute_all_signatures(root_learnt,num_gens_to_check=num_gens_to_check)
+
+    if 0:
+        sigs_l=np.load("/tmp/sigs_l.npy")
     
-    if 1:
-        import pickle
-        #with open("/home/achkan/misc_experiments/guidelines_log/signature_computation/sig_learnt_5000.pickle","rb") as fl:
-        #with open("/tmp/sigs_l_learnt_lower_LR.pickle","rb") as fl:
-        with open("/tmp/sigs_l_with_divergent_training.pickle","rb") as fl:
-            sigs_l=pickle.load(fl)
-
-                    
-        for y in sigs_l:
-            plt.plot(y,"b")
-        plt.show()
-
-        
-        #for x,y in zip(seen_l, seen_a):
-        #    X=np.concatenate([np.array(a).reshape(1,2) for a in x],0)
-        #    Y=np.concatenate([np.array(a).reshape(1,2) for a in y],0)
-        #    plt.plot(X[:,0],X[:,1],"b")
-        #    plt.plot(Y[:,0],Y[:,1],"r")
-        #    plt.show()
 
     sigs_a=np.concatenate([np.array(x).reshape(1,-1) for x in sigs_a],0)
     sigs_l=np.concatenate([np.array(x).reshape(1,-1) for x in sigs_l],0)
 
-    #sigs_l=sigs_l[:num_gens_to_check,:num_gens_to_check]
-    
-    #this one was computed on remote server
-    #sigs_a_from_archive_time_2000=[24.81793639678651, 25.78711511336961, 140.0409259832257, 1524.322705715413, 1190.6048688310264, 773.4165719149466]
-
-    mu.plot_with_std_band(range(6),sigs_a.mean(0),sigs_a.std(0),hold_on=True,label="Archive of size 6000", only_positive=False,color="red")
-    mu.plot_with_std_band(range(6),sigs_l.mean(0),sigs_l.std(0),hold_on=True,label="BR-NS", only_positive=False,color="blue")
+    mu.plot_with_std_band(range(sigs_a.shape[1]),sigs_a.mean(0),sigs_a.std(0),hold_on=True,label="Archive of size 6000", only_positive=False,color="red")
+    mu.plot_with_std_band(range(sigs_l.shape[1]),sigs_l.mean(0),sigs_l.std(0),hold_on=True,label="BR-NS", only_positive=False,color="blue")
     plt.show()
 
-    #plt.plot(sigs_a.std(0),"r")
-    #plt.plot(sigs_a_from_archive_time_2000,color="orange")
-    #plt.plot(sigs_l.std(0),"b")
-    #plt.show()
+    plt.plot(sigs_a.std(0),"r")
+    plt.plot(sigs_l.std(0),"b")
+    plt.show()
 
 
-    cover_a, js_a, grid_a=check_signatures_cover_and_uniformity(sigs_a.transpose(),Gs=[3,3,5,20,15,10])
-    cover_l, js_l, grid_l=check_signatures_cover_and_uniformity(sigs_l.transpose(),Gs=[3,3,5,20,15,10])
+    #cover_a, js_a, grid_a=check_signatures_cover_and_uniformity(sigs_a.transpose(),Gs=[3,3,5,20,15,10])
+    #cover_l, js_l, grid_l=check_signatures_cover_and_uniformity(sigs_l.transpose(),Gs=[3,3,5,20,15,10])
 
 
 

AnalysisScripts/cycling_analysis.py
@@ -22,41 +22,13 @@ sys.path.append("..")
 from NS import MiscUtils
 
 
-def kappa(QM,n=200):
-    """
-    Computes the kappa measure as defined in BR-NS
-
-    QM np.array of size g*g where g is the number of generations
-       QM[i,j] should contain the novelty of population i computed using the novelty estimator of generation j (which can either be an archive or a learnt novelty estimator)
-
-    Kappa[i] is then defined as the ratio eta_1/eta_0 where eta_0 is the mean novelty of the population and eta_1 the maximum mean novelty of the population after it has started rising again, i.e.
-      after its minimum novelty has been reached. For an ideal archive, this should be zero
-    """
-
-    res=[]
-    #for i in range(QM.shape[0]):
-    #    am=QM[i,i:].argmax()
-    #    zz=QM[i,am+i:].argmin()
-    #    vv=QM[i,zz+am+i:]
-    #    #pdb.set_trace()
-    #    ratios.append(vv.max()/QM[i,am+i])
-    #    #ratios.append(sum(vv>QM[i,i]))
-
-    for i in range(QM.shape[0]):
-        zz=QM[i,i:].argmin()
-        #vv=QM[i,i+zz:]
-        res.append(sum(QM[i,i+1:]>zz+4.8))#for br-ns (note that these values were obtained as explained in the paper)
-        #res.append(sum(QM[i,i+1:]>zz+17))#for archive (note that these values were obtained as explained in the paper)
-    
-    return res
-
-
 
 def analyse_cycling_behavior_archive_based(root_dir):
 
     ################################################# load all populations and archives
 
-    generations_to_use=list(range(1,1300,10))
+    #do skip at least 5 as otherwise it takes ages to compute 
+    generations_to_use=list(range(1,1000,10))
     populations=[]#those will actually store behavior descriptors
     archives=[]
     unused=[]#this will store the indexes of the first few geneartions where ther archive is empty
@@ -116,8 +88,7 @@ def analyse_cycling_behavior_learnt_nov(root_dir, in_dim, out_dim):
     frozen.load_state_dict(torch.load(frozen_net_path))
     frozen.eval()
 
-    learned_model_generations=list(range(0,1060,1))
-    #learned_model_generations=list(range(0,900,1))
+    learned_model_generations=list(range(0,2000,1))
     learned_models_paths=[root_dir+f"/learnt_{i}.model" for i in learned_model_generations]
     num_non_frozen=len(learned_models_paths)
     models=[]
@@ -175,7 +146,6 @@ def analyse_cycling_behavior_learnt_nov(root_dir, in_dim, out_dim):
                 #novelty=diff.median().item()
                 Qmat[i,j]=novelty
 
-            s_i=Qmat[i,:].max()
             #Qmat[i,:]/=s_i
 
 
@@ -190,10 +160,10 @@ def analyse_cycling_behavior_learnt_nov(root_dir, in_dim, out_dim):
 if __name__=="__main__":
     
    
-    if 1:
+    if 0:
         root="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/learnt/"
         #root="/tmp"
-        root="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/"
+        root="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/learnt"
         experiment_names=os.listdir(root)
 
         #experiment_names.pop(experiment_names.index("NS_log_67193"))
@@ -215,30 +185,32 @@ if __name__=="__main__":
             #qm=analyse_cycling_behavior_learnt_nov(list_of_experiments[i], in_dim=2, out_dim=4)
             qmats.append(qm)
 
-        plt.imshow(qm);plt.show()
+        #plt.imshow(qm);plt.show()
 
         #xx, yy= np.meshgrid(range(qm.shape[0]), range(qm.shape[0]))
         #fig=plt.figure(); ax=fig.add_subplot(111, projection='3d'); ax.plot_surface(yy,xx,np.sqrt(qm),cmap=cm.coolwarm); plt.show()
 
-    if 0:
+    if 1:
         
         #root="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/"
         #root="/home/achkan/misc_experiments/guidelines_log/cycling_behavior/"
-        root="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/archive_4000/"
+        #root="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/archive_4000/"
+        root="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/archive_based/"
         
-        #experiment_names=os.listdir(root)
+        experiment_names=os.listdir(root)
 
 
-        #list_of_experiments=[]
-        #for x in experiment_names:
-        #    if "NS_log_" not in x:
-        #        continue
-        #    else:
-        #        list_of_experiments.append(root+"/"+x)
+        list_of_experiments=[]
+        for x in experiment_names:
+            if "NS_log_" not in x:
+                continue
+            else:
+                list_of_experiments.append(root+"/"+x)
 
 
         #list_of_experiments=list_of_experiments[:1]
-        list_of_experiments=[root+"/"+"NS_log_87678"]
+
+        print(list_of_experiments)
    
         qmats=[]
         for i in range(len(list_of_experiments)):

NS/LargeAntMaze.py
@@ -68,6 +68,12 @@ class LargeAntMaze(Problem):
 
     def get_bd_dims(self):
         return self.bd_extractor.get_bd_dims()
+    
+    def get_behavior_space_boundaries(self):
+        lam_limits=np.array([[-50,50]])
+        lam_limits=np.repeat(lam_limits, self.get_bd_dims(), axis=0)
+        return lam_limits
+
 
     def __call__(self, ag):
         """
@@ -253,17 +259,21 @@ if __name__=="__main__":
 
 
 
-        #root_dir="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/learnt/NS_log_11048/"
-        #root_dir="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/learnt/NS_log_67193"
         
-        #root_dir="/home/achkan/misc_experiments/guidelines_log/ant/32d-bd/archive_4000//NS_log_87678"
-        root_dir="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/NS_log_1605/"
+        #root_dir="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/learnt/NS_log_1605/"
+       
+        root_dir="/home/achkan/misc_experiments/guidelines_log/for_open_source_code/large_ant/archive_based//NS_log_34498"
 
         
 
-        gen_to_load=range(0,2000,1)
+        #gen_to_load=range(800,801,1)
+        gen_to_load=range(0,1400,1)
         bds_list=set()
-        
+      
+        append_path=[[0,-24]]#because of the way the sampling of the path is done, the initial point at which the ant is spawned is missing
+        append_path=[np.array(x).reshape(1,-1) for x in append_path]
+        to_append=np.concatenate(append_path,0)
+
         for i in gen_to_load:
             if i%10==0:
                 print("i==",i)
@@ -285,14 +295,22 @@ if __name__=="__main__":
                     bds=[x.reshape(num_pts,2) for x in bds]
 
                     for bd in bds:
+                        bd_full=np.concatenate([bd, to_append], 0)
                         for j in range(len(dummy.env.goals)):
                             goal_j=dummy.env.goals[j]
                             plt.plot(goal_j.coords[0],goal_j.coords[1],color=goal_j.color,marker="o",linestyle="",markersize=25)
 
 
-                        plt.plot(bd[:,0],bd[:,1],"k-")
-                    #plt.show()
-        plt.show()
+                        #plt.plot(bd_full[:,0],bd_full[:,1],"k-")#path
+                        #plt.plot(bd_full[-1,0],bd_full[-1,1],"yo")#starting point
+                        plt.plot(bd_full[0,0],bd_full[0,1],"mo")#end point
+                        #plt.show()
+        
+        plt.xlim(-45,45)
+        plt.ylim(-45,45)
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.savefig("/tmp/cover_fig.png")
+        #plt.show()
 
         M=np.concatenate([np.array(x).reshape(1,32) for x in bds_list],0)
         cov=np.cov(M.transpose())
@@ -306,6 +324,8 @@ if __name__=="__main__":
             val*=x
 
         print(val)
+
+
 
 
     

NS/NoveltyEstimators.py
@@ -94,7 +94,7 @@ class ArchiveBasedNoveltyEstimator(NoveltyEstimator):
 
 class LearnedNovelty1d(NoveltyEstimator):
 
-    def __init__(self, in_dim, emb_dim, batch_sz=128, log_dir="/tmp/"):
+    def __init__(self, in_dim, emb_dim, pb_limits=None, batch_sz=128, log_dir="/tmp/"):
 
         self.frozen=MiscUtils.SmallEncoder1d(in_dim,
             emb_dim,
@@ -122,17 +122,8 @@ class LearnedNovelty1d(NoveltyEstimator):
         self.log_dir=log_dir
 
 
-        deceptive_maze=False
-        large_ant_maze=True
-        if deceptive_maze:
-            hm_limits=np.array([[0,600],[0,600]])
-            MiscUtils.make_networks_divergent(self.frozen, self.learnt, hm_limits, iters=50)
-        elif large_ant_maze:
-            lam_limits=np.array([[-50,50]])
-            lam_limits=np.repeat(lam_limits, self.frozen.in_d, axis=0)
-            MiscUtils.make_networks_divergent(self.frozen, self.learnt, lam_limits, iters=50)
-        else:
-            raise Exception("shouldn't happen")
+        if pb_limits is not None:
+            MiscUtils.make_networks_divergent(self.frozen, self.learnt, pb_limits, iters=50)
 
         
     def update(self, pop, archive=None):